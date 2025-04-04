570e52316afa2ccd3dccf47f596d151cb2afd054
sachinnnsingh0510@gmail.com <105480714+me-sachin-singh@users.noreply.github.com>
2025-04-04 15:09:07 +0530

    update

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+	apiKey: "AIzaSyCL6DVhGEXrR_FdWHIbgX1I1o02dxjLIHk",
+	authDomain: "prepwave-a5248.firebaseapp.com",
+	projectId: "prepwave-a5248",
+	storageBucket: "prepwave-a5248.firebasestorage.app",
+	messagingSenderId: "132903657952",
+	appId: "1:132903657952:web:cbceab847f358f3e384b85",
+	measurementId: "G-KYP6NDVF3C",
 };
 
 // Initialize Firebase
